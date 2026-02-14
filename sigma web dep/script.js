28658fd3d8af1c3e7318836839e0e73720d5381c
Ishasolanki7 <isha639686@gmail.com>
2026-02-14 14:59:53 +0530

    Delete sigma web dep directory

--- sigma web dep/script.js
+++
@@ -1,9 +0,0 @@
-function validateForm() {
-  let name = document.getElementById("name").value;
-  if (name == "") {
-    alert("Please enter your name");
-    return false;
-  }
-  alert("Table Booked Successfully!");
-  return true;
-}
